3eb22aca8bb14365c99d6f159aa971af0c80efd5
andrewdeveloper12 <andrewdematry@gmail.com>
2025-05-08 18:50:35 +0300

    Updates

--- assets/icons-v_SoCzJd.js
+++ assets/icons-v_SoCzJd.js
@@ -1,4 +1,4 @@
-import{r as s}from"./react-nf7bT_Uh.js";/**
+import{r as s}from"./react-Gm9i_4Ku.js";/**
  * @license lucide-react v0.487.0 - ISC
  *
  * This source code is licensed under the ISC license.
